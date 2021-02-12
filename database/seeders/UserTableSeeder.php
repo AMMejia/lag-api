6c0254823704628e0597da066bf3110392385d26
Alex Mejia <amejia@transom-group.com>
2021-02-12 11:40:43 -0600

    aadding role to user

---
+++ database/seeders/UserTableSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::truncate();
+        User::create([
+            'email' => 'admin@lag.com',
+            'password' => Hash::make('adminadmin'),
+            'name' => 'Super Usuario Lag',
+            'role' => 'owner',
+        ]);
+    }
+}
